72640f9afe4b2b99ab6bb39765f2029fc35275cc
Maniakalochka <prochorwolochoff@gmail.com>
2025-09-06 21:36:38 +0300

    refactor: improve import formatting and remove unused TEST_DB_URL logic

--- src/databases/db.py
+++ src/databases/db.py
@@ -1,18 +1,15 @@
-from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
-                                    async_sessionmaker, create_async_engine)
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.pool import NullPool
 
 from core.config import settings
 
 DATABASE_URL = settings.DB_URL
 DATABASE_PARAMS = {"poolclass": NullPool}
-
-if settings.MODE == "TEST":
-    DATABASE_URL = settings.TEST_DB_URL
-    DATABASE_PARAMS = {"poolclass": NullPool}
-else:
-    DATABASE_URL = settings.DB_URL
-    DATABASE_PARAMS = {"poolclass": NullPool}
 
 
 engine: AsyncEngine = create_async_engine(
